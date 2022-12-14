db1fe4d2c1711dd4cb4f3eeb88dee9636f8ae952
Austin Stancil <117120566+AustinS86@users.noreply.github.com>
2022-12-14 17:58:03 -0500

    Added my JS, CSS, HTML pages and added questions

---
+++ Assets/questions.js
@@ -0,0 +1,49 @@
+var questions = [
+
+    {
+        question: "The condition in an if / else statement is enclosed within ____.",
+        choices:["quotes", "curly brackets", "parentheses", "square brackets"],
+        correctanswer: "parentheses"
+    }
+
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        correctanswer: "alerts"
+    }
+
+    {
+        question: "Arrays in Javascript can be used to store___",
+        choices:"numbers & strings, other arrays,bBooleans, all of the ablove",
+        correctanswer: "all of the above"
+    }
+
+    {
+        question: "Where would you store the JS tag in the HTML?"
+        choices:"<head>, <body>, <footer>,<meta>"
+        correctanswer: "<body>"
+    }
+
+    {
+        question: 
+        choices:
+        correctanswer: ""
+    }
+
+    {
+        question: ""
+        choices:""
+        correctanswer: ""
+    }
+
+    
+
+
+
+
+
+
+
+
+
+]
