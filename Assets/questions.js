8a7ce7cf20b2464b062eb75884a40d6d5796de48
Austin Stancil <117120566+AustinS86@users.noreply.github.com>
2022-12-16 20:14:39 -0500

    added style in CSS, created a main page in HTML

--- Assets/questions.js
+++ Assets/questions.js
@@ -4,37 +4,33 @@ var questions = [
         question: "The condition in an if / else statement is enclosed within ____.",
         choices:["quotes", "curly brackets", "parentheses", "square brackets"],
         correctanswer: "parentheses"
-    }
+    },
 
     {
         question: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
-        correctanswer: "alerts"
-    }
-
+        correctanswer: "alerts",
+    },
     {
         question: "Arrays in Javascript can be used to store___",
         choices:"numbers & strings, other arrays,bBooleans, all of the ablove",
-        correctanswer: "all of the above"
-    }
+        correctanswer: "all of the above",
+    },
 
     {
-        question: "Where would you store the JS tag in the HTML?"
-        choices:"<head>, <body>, <footer>,<meta>"
+        question: "What HTML tag would you put the JS <scripted> tag under?",
+        choices:["<head>, <body>, <footer>, <meta>"],
         correctanswer: "<body>"
-    }
+    },
 
     {
-        question: 
-        choices:
-        correctanswer: ""
-    }
+        question: "String values must be enclosed within ____ when being assigned to variables",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        correctanswer: "quotes"
+    },
 
-    {
-        question: ""
-        choices:""
-        correctanswer: ""
-    }
+]
+
 
     
 
