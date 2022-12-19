7f2ebe0e191afde12f62276e0ec4387560c483bd
Austin Stancil <117120566+AustinS86@users.noreply.github.com>
2022-12-19 10:46:10 -0500

    added more style to CSS, added more var in my JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,8 @@
 var startBtn = document.getElementById("startBtn");
-var time = 75;
+var time = 60;
 var timeLeft = true;
 var timeStart = false;
-var timer = document.getElementById("timer");
+var countdownTimer = document.getElementById("countdownTimer");
 var mainscreen = document.getElementById("main-screen");
 var questionList = document.getElementById("question-list");
 var choices0 = document.getElementById("choices0");
@@ -16,12 +16,12 @@ var score = 0;
 var i = 0;
 
 
-var TimerInterval = setInterval(setTimer, 1000);
-function TimerInterval() {
+var countdownTimerInterval = setInterval(setcountdownTimer, 1000);
+function setcountdownTimer() {
     if (timeStart)
         time--;
     if (time <= 0) {
-        end_questionnaire();
+        end_questions();
         time = 0;
     }
     document.getElementById("time-left").innerHTML = time;
@@ -31,50 +31,221 @@ var questions = [
 
     {
         question: "The condition in an if / else statement is enclosed within ____.",
-        choices:["0) quotes", "1) curly brackets", "2) parentheses", "3) square brackets"],
-        correctanswer: "parentheses"
+        choices: [" quotes", "curly brackets", " parentheses", " square brackets"],
+        correctAnswer: 2
     },
 
     {
         question: "Commonly used data types DO NOT include:",
-        choices: ["0) strings", "1) booleans", "2) alerts", "3) numbers"],
-        correctanswer: "alerts",
+        choices: [" strings", " booleans", " alerts", " numbers"],
+        correctAnswer: 2
     },
     {
         question: "Arrays in Javascript can be used to store___",
-        choices:"0) numbers & strings, 1) other arrays,2) booleans, 3) all of the above",
-        correctanswer: "all of the above",
+        choices: [" numbers & strings",  "other arrays", "booleans",  "all of the above"],
+        correctAnswer: 3
     },
 
     {
         question: "What HTML tag would you put the JS <scripted> tag under?",
-        choices:["0) <head>, 1) <body>, 2) <footer>, 3) <meta>"],
-        correctanswer: "<body>"
+        choices: [" <head>",  "<body>",  "<footer>",  "<meta>"],
+        correctAnswer: 1
     },
 
     {
         question: "String values must be enclosed within ____ when being assigned to variables",
-        choices: ["0) commas", "1) curly brackets", "2) quotes", "3) parentheses"],
-        correctanswer: "quotes"
+        choices: [" commas", "curly brackets", " quotes", " parentheses"],
+        correctAnswer: 2
     },
 
 ]
 
-startBtn.addEventListener("click", function() {
+startBtn.addEventListener("click", function(){
     questionnaire.style.display = "block";
-    main-screen.style.display ="none";
-    timer.style.display= "block";
-    document.getElementById("score-total").style.display= "block";
+    //mainscreen.style.display = "none";//
+    countdownTimer.style.display = "block";
+    document.getElementById("score-total").style.display = "block";
     document.getElementById("score").innerHTML = score;
-    setCountdownTimer();
-    setQuizQuestions();
-    timeStart= true;
+    setcountdownTimer();
+    setQuestionnairequestions();
+    timeStart = true;
 });
 
 function setQuestionnairequestions() {
     questionList.textContent = questions[i].question;
-    choices0.textContent = questions[i].choices[0]; 
-    choices1.textContent = questions[i].choices[1]; 
-    choices2.textContent = questions[i].choices[2]; 
-    choices3.textContent = questions[i].choices[3]; 
+    choices0.textContent = questions[i].choices[0];
+    choices1.textContent = questions[i].choices[1];
+    choices2.textContent = questions[i].choices[2];
+    choices3.textContent = questions[i].choices[3];
+};
+
+choices0.addEventListener("click", function (event) {
+    event.stopPropagation();
+    correctAnswer = questions[i].correctAnswer;
+    console.log("correctAnswer" + correctAnswer);
+
+    if (0 === correctAnswer) {
+        document.getElementById("Answer").innerHTML = "Correct! Next question.";
+        setTimeout(function () {
+            document.getElementById("Answer").innerHTML = "";
+        },
+            1000
+        );
+        score++;
+        document.getElementById("final-score").innerHTML = score;
+
+    } else {
+        timeLeft -= 10;
+        document.getElementById("Answer").innerHTML = "Wrong! Next Question.";
+        setTimeout(function () {
+            document.getElementById("Answer").innerHTML = "";
+        },
+            1000
+        );
+    }
+    if (i >= questions.length - 1) {
+        end_questions();
+    } else {
+        i++
+        setQuestionnairequestions();
     };
+});
+
+choices1.addEventListener("click", function (event) {
+    event.stopPropagation();
+    correctAnswer = questions[i].correctAnswer;
+    console.log("correctAnswer" + correctAnswer);
+
+    if (1 === correctAnswer) {
+        document.getElementById("Answer").innerHTML = "Correct! Next question.";
+        setTimeout(function () {
+            document.getElementById("Answer").innerHTML = "";
+        },
+            1000
+        );
+        score++;
+        document.getElementById("final-score").innerHTML = score;
+
+    } else {
+        timeLeft -= 10;
+        document.getElementById("Answer").innerHTML = "Wrong! Next Question.";
+        setTimeout(function () {
+            document.getElementById("Answer").innerHTML = "";
+        },
+            1000
+        );
+    }
+    if (i >= questions.length - 1) {
+        end_questions();
+    } else {
+        i++
+        setQuestionnairequestions();
+    };
+});
+
+choices2.addEventListener("click", function (event) {
+    event.stopPropagation();
+    correctAnswer = questions[i].correctAnswer;
+    console.log("correctAnswer" + correctAnswer);
+
+    if (2 === correctAnswer) {
+        document.getElementById("Answer").innerHTML = "Correct! Next question.";
+        setTimeout(function () {
+            document.getElementById("Answer").innerHTML = "";
+        },
+            1000
+        );
+        score++;
+        document.getElementById("final-score").innerHTML = score;
+
+    } else {
+        timeLeft -= 10;
+        document.getElementById("Answer").innerHTML = "Wrong! Next Question.";
+        setTimeout(function () {
+            document.getElementById("Answer").innerHTML = "";
+        },
+            1000
+        );
+    }
+    if (i >= questions.length - 1) {
+        end_questions();
+    } else {
+        i++
+        setQuestionnairequestions();
+    };
+});
+
+choices3.addEventListener("click", function (event) {
+    event.stopPropagation();
+    correctAnswer = questions[i].correctAnswer;
+    console.log("correctAnswer" + correctAnswer);
+
+    if (3 === correctAnswer) {
+        document.getElementById("Answer").innerHTML = "Correct! Next question.";
+        setTimeout(function () {
+            document.getElementById("Answer").innerHTML = "";
+        },
+            1000
+        );
+        score++;
+        document.getElementById("final-score").innerHTML = score;
+
+    } else {
+        timeLeft -= 10;
+        document.getElementById("Answer").innerHTML = "Wrong! Next Question.";
+        setTimeout(function () {
+            document.getElementById("Answer").innerHTML = "";
+        },
+            1000
+        );
+    }
+    if (i >= questions.length - 1) {
+        end_questions();
+    } else {
+        i++
+        setQuestionnairequestions();
+    };
+});
+
+function end_questions() {
+    document.getElementById("end").style.display = "block";
+    document.getElementById("questionnaire").style.display = "none";
+    document.getElementById("countdownTimer").style.display = "none";
+    document.getElementById("score-total").style.display = "none";
+    document.getElementById("Answer").innerHTML = "";
+    document.getElementById("final-score").innerHTML = score;
+}
+
+function submit_score() {
+    highScores.push(document.getElementById("name").value + "" + score);
+    view_high_scores();
+}
+
+document.getElementById("questionnaire").style.display = "none";
+document.getElementById("end").style.display = "none";
+document.getElementById("view-high-scores").style.display = "block";
+
+output = "";
+for (let i = 0; i < highScores.length; i++) {
+    output = output + "" + highScores[i];
+}
+document.getElementById("high-scores").innerHTML = output;
+clear();
+
+function home() {
+    document.getElementById("view-high-scores").style.display = "none";
+    document.getElementById("mainscreen").style.display = "block";
+    clear();
+}
+
+function clear_sc() {
+    highScores = [];
+}
+
+function clear() {
+    time = 60;
+    timeLeft = true;
+    timeStart - false;
+    i = 0
+    score = 0;
+}
